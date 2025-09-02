ac49ead628798ebaf526fdac7f5118018c6832e5
Gustavo Honorio <gustavoribeirohonorio@hotmail.com>
2025-09-02 11:15:34 -0300

    Projeto Gerado com Spring Initializr

---
+++ src/test/java/br/com/processmanager/api/ProcessManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.processmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
